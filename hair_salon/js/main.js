ed3fe5eb3848011b63152c035ca5c5fc0c036cb5
Parth Patel <163419505+parth0512@users.noreply.github.com>
2025-02-10 16:24:59 +0530

    Add files via upload

---
+++ hair_salon/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let navbarToggler = document.querySelector(".navbar-toggler");
+    let navbarCollapse = document.querySelector(".navbar-collapse");
+    let background = document.querySelector(".background");
+  
+    navbarToggler.addEventListener("click", function () {
+      if (!navbarCollapse.classList.contains("show")) {
+        background.style.marginTop = "0px"; 
+      } else {
+        
+        background.style.marginTop = "0";
+      }
+    });
+  
+    navbarCollapse.addEventListener("hidden.bs.collapse", function () {
+      background.style.marginTop = "0"; 
+    });
+  });
+  
